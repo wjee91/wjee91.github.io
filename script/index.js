8a131066823f10d9269103fdfd729b8354b80047
Wenjun Cai <wjee91@gmail.com>
2025-01-02 18:12:19 -0800

    Update index.js

--- script/index.js
+++ script/index.js
@@ -25,7 +25,7 @@ function animateElement(element, settings) {
 }
 
 function typing(note) {
-    var s = "Hello 2024!🌈",
+    var s = "Hello 2025!🌈",
         n = s.length, i = -1, l = [0, n];
 
     typingTimer = setInterval(function () {
